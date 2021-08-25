b5c8d6668146fc5a531c098b1b6f4a0fd701bf22
hmendozaq <hmendozaq@DESKTOP-0HLSANU>
2021-08-24 19:57:22 -0500

    Agregar archivos de proyecto.

---
+++ Clase01/Clases/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase01.Clases
+{
+    class Persona
+    {
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Genero { get; set; }
+        public int Estatura { get; set; }
+        public float Peso { get; set; }
+        public bool TieneEfermedades { get; set; }
+    }
+}
---
+++ Clase01/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Buenas practicas
+             1.- Las clases deben estar en nomenclatura camello alta y en singular
+            2.- Poner en carpetas los diferentes elementos para organizarlos dependiendo de su funcionamiento
+            3.- Las clases de forma predeterminada son privadas
+            4.- Para rearlizar una instancia es de la siguiente forma: var nombre = new Clase();
+             */
+
+            var persona = new Clases.Persona(); //Una instancia de un objeto F1
+            //Clases.Persona persona = new Clases.Persona(); // F2 de hacer una instancia
+            //Para asignar un valor a una propiedad este debe estar a la izquierda del =
+            Console.Write("Escribe tu nombre: ");
+            persona.Nombre = Console.ReadLine();
+
+            Console.Write("Escribe tu estatura: ");
+            //persona.Estatura = int.Parse(Console.ReadLine());
+            persona.Estatura = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Escribe tu peso: ");
+            persona.Peso = float.Parse(Console.ReadLine());
+
+            Console.Write("Escribe tu fecha de nacimiento: ");
+            persona.FechaNacimiento = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Tienes enfermedades (Si/NO): ");
+            var respuesta = Console.ReadLine();
+
+            if (respuesta.ToUpper() == "SI")
+                persona.TieneEfermedades = true;
+            else
+                persona.TieneEfermedades = false;
+
+            //persona.TieneEfermedades = (respuesta == "SI") ? true : false; // Operador ternario                
+
+            Console.WriteLine("Tu nombre es: " + persona.Nombre);
+            Console.WriteLine("Tu nombre es: " + persona.Nombre.ToLower());
+            Console.WriteLine("Tu nombre es: " + persona.Nombre.ToUpper());
+            Console.WriteLine("Tu nombre es: " + persona.Nombre.Trim());
+            Console.WriteLine("Tu nombre sin los ultimos 4 caracteres es: " + persona.Nombre.Substring(0, persona.Nombre.Length - 4));
+            Console.WriteLine("Tu nombre es: " + persona.Nombre.Substring(0, 4).ToUpper());
+            Console.WriteLine("Tu nombre tiene " + persona.Nombre.Length + " letras");
+
+            Console.WriteLine("Tu estatura es: " + persona.Estatura);
+            Console.WriteLine("Tu peso es: " + persona.Peso);
+            Console.WriteLine("Tu fecha de nacimiento es: " + persona.FechaNacimiento);
+            Console.WriteLine("Tienes enfermedades: " + persona.TieneEfermedades);
+
+            Console.ReadKey();
+        }
+    }
+}
