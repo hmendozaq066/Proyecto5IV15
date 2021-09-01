804c4768dad17d99c4262c4cefbd2e42fe5d6293
HECTOR MANUEL MENDOZA QUIROZ <hmendoza@ipn.mx>
2021-08-31 20:56:32 -0500

    0856

---
+++ Clase02/Clases/Complejo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase02.Clases
+{
+    class Complejo
+    {
+        //Propiedades o Atributos
+        public int A { get; set; }
+        public int B { get; set; }
+
+
+        /*
+         * Constructores
+         * A constructor in C# is a member of a class. It is a method in the class which 
+         * gets executed when a class object is created. Usually we put the initialization 
+         * code in the constructor. The name of the constructor is always is the same 
+         * name as the class. A C# constructor can be public or private.
+         * 
+         * Sintaxis
+         * public|private NombreDeLaClase (parámetros) { código }
+         */
+
+        public Complejo(int A, int b)
+        {
+            System.Console.WriteLine("Ejecutando el contructor con 2 parámetros");
+            /*
+             * this
+             * The C# “this” keyword represents the “this” pointer of a class or a struct. 
+             * The this pointer represents the current instance of a class or struct.
+             */
+            this.A = A; //Cuando la propiedad se llama igual que la variable local se debe usar this
+            this.B = b;//En caso contrario es opcional
+        }
+
+        //¿Como sabemos que esta sobrecargado? R= porque el método o constructor se llaman igual pero tiene diferentes parámetros
+
+        public Complejo()
+        {
+            /*
+             * Como hemos visto el constructor es un método y como tal podemos sobrecargarlo, 
+             * es decir definir varios constructores con distintas cantidades o tipos de parámetros.
+             */
+            Console.WriteLine("Ejecutando con el constructor sin parámetros");
+            A = 0;
+            B = 0;//Aquí no se utiliza this porque no hay otras variables locales con el mismo nombre
+        }
+
+        /*
+         * Suma de numeros complejos
+         * z = 5 + 4i  
+         * y = 6 + 10i
+         * 
+         * z + y = (5 + 6) + (4 + 10)i = 11 + 14i
+         * 
+         */
+
+        public Complejo Suma(Complejo complejo)
+        {
+            var resultado = new Complejo();
+            resultado.A = A + complejo.A;
+            resultado.B = B + complejo.B;
+            return resultado;
+        }
+
+        public string FormaBinomica()
+        {
+            return string.Format("{0} + {1}i", A, B);
+        }
+
+    }
+}
---
+++ Clase02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase02.Clases;
+
+namespace Clase02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var complejo = new Complejo(1, 2);//Aquí estamos estableciendo la instancia de la clase complejo
+
+            Console.WriteLine("El valor de A es " + complejo.A);
+            Console.WriteLine("El valor de B es " + complejo.B);
+            Console.WriteLine("La forma binomica es " + complejo.FormaBinomica());
+
+            var complejo2 = new Complejo(10, 5);
+
+            Console.WriteLine("El valor de A es " + complejo2.A);
+            Console.WriteLine("El valor de B es " + complejo2.B);
+            Console.WriteLine("La forma binomica es " + complejo2.FormaBinomica());
+
+            var resultado = complejo.Suma(complejo2);
+            Console.WriteLine("La forma binomica es " + resultado.FormaBinomica());
+
+            Console.ReadKey();
+        }
+    }
+}
